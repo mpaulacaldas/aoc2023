22170815941bd3083c6bdf8eae22581564873e29
María Paula Caldas <mpaulacaldas@gmail.com>
2023-12-04 11:24:10 +0100

    First attempt at day 4

---
+++ R/day04.R
@@ -0,0 +1,19 @@
+add_points_scratchcards <- function(input) {
+  input |>
+    str_remove_all("Card \\d+: ") |>
+    read_delim(
+      col_names = c("winning", "mine"),
+      col_types = cols(.default = col_character()),
+      delim = " | ") |>
+    mutate(card = row_number()) |>
+    mutate(across(
+      c(winning, mine),
+      \(x) str_extract_all(x, "\\d+")
+    )) |>
+    rowwise() |>
+    mutate(n = sum(mine %in% winning)) |>
+    ungroup() |>
+    mutate(points = ifelse(n > 0, 2 ^ (n - 1), 0)) |>
+    summarise(sum(points)) |>
+    pull()
+}
